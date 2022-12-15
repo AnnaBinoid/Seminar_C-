49a9bb7e487c93d479f269247b3014af45ec044b
Anna_Zagrebina <eto-projdet@yandex.ru>
2022-12-15 22:51:18 +0300

    Add Task003_Week & Task007_3numb

---
+++ Task003_Week/Program.cs
@@ -0,0 +1,42 @@
+﻿//3. Напишите программу, которая будет выдавать
+//название дня недели по заданному номеру.
+//3 -> Среда
+//5 -> Пятница
+
+Console.Write("Write the number from 1 to 7, please: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+if (number < 7 && number > 0)
+{
+    if (number == 1)
+    {
+        Console.WriteLine("It's Monday!");
+    }
+    if (number == 2)
+    {
+        Console.WriteLine("It's Tuesday!");
+    }
+    if (number == 3)
+    {
+        Console.WriteLine("It's Wednesday!");
+    }
+    if (number == 4)
+    {
+        Console.WriteLine("It's Thursday!");
+    }
+    if (number == 5)
+    {
+        Console.WriteLine("It's Friday!");
+    }
+    if (number == 6)
+    {
+        Console.WriteLine("It's Saturday!");
+    }
+    if (number == 7)
+    {
+        Console.WriteLine("It's Sunday!");
+    }
+}
+else
+{ Console.WriteLine("Please, read conditions carefully! Next time write the number from 1 to 7."); }
