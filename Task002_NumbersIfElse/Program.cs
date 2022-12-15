dc6ce6a49d1592e18a9804bfb9cca83c986f8a7a
Anna_Zagrebina <eto-projdet@yandex.ru>
2022-12-15 21:46:20 +0300

    Add Task002_Number

---
+++ Task002_NumbersIfElse/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Write the number, please: ");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Write the number, please: ");
+int numberTwo = Convert.ToInt32(Console.ReadLine());
+
+int squareOne = numberOne * numberOne;
+
+Console.WriteLine(numberOne);
+Console.WriteLine(numberTwo);
+
+if (numberTwo == squareOne)
+{
+    Console.WriteLine("It's a sqare!");
+}
+else Console.WriteLine("It's not a sqare!");
