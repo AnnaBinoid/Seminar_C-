a824ff2ea88b992915ce972bb2a381b4f8f14cea
Anna_Zagrebina <eto-projdet@yandex.ru>
2022-12-15 21:14:06 +0300

    Add Task001

--- Task001/Program.cs
+++ Task001/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишшите программу, которая
+//На вход принмает число и
+//2. Выдает его квадрат (число, умноженное на себя).
+
+Console.Write("Write the number, please: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int square = number * number;
+
+Console.WriteLine($"Good question! The square of {number} is: {square}!");
