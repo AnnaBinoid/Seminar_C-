1336e5aec56c70853cf44091839048d1913a4e54
Anna_Zagrebina <eto-projdet@yandex.ru>
2022-12-16 23:18:25 +0300

    Rename Task004HW

---
+++ Task004HW_maxFrom3numbers/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+Task0004HW_maxFrom3numbersConsole.WriteLine("Enter three integers separated by Enter!");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+int numberTwo = Convert.ToInt32(Console.ReadLine());
+int numberThree = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Your first integer is {numberOne}, your second integer is {numberTwo}, your third integer is {numberThree}!");
+int max = numberOne;
+
+if (max > numberTwo)
+{
+    if (max <= numberThree)
+    {
+        max = numberThree;
+    }
+}
+if (max <= numberTwo)
+{
+    max = numberTwo;
+    if (max <= numberThree)
+    {
+        max = numberThree;
+    }
+}
+Console.WriteLine($"Max from them is {max}!");
