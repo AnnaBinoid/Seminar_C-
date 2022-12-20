aafb1f634d1b650d0de205430fcdd21147fdd4f3
Anna_Zagrebina <eto-projdet@yandex.ru>
2022-12-20 19:48:32 +0300

    Fixed Format Task016_OneSquareTwo/

---
+++ Task016_OneSquareTwo/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+16. Напишите программу, которая принимает на
+вход два числа и проверяет, является ли одно
+число квадратом другого.
+5, 25 -> да
+-4, 16 -> да
+25, 5 -> да
+8,9 -> нет
+*/
+
+Console.WriteLine("Enter the first number :");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the second number : ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(SquareNumber(firstNumber, secondNumber) ? "Yes!" : "No!");
+
+bool SquareNumber(int num1, int num2)
+{
+    return num1 == num2 * num2 || num2 == num1 * num1;
+
+}
