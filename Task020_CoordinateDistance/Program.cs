fdac4ed82788f50b0d6137424de68f1ca68591ce
Anna_Zagrebina <eto-projdet@yandex.ru>
2022-12-25 23:34:43 +0300

    All changes

--- Task020_CoordinateDistance/Program.cs
+++ Task020_CoordinateDistance/Program.cs
@@ -8,20 +8,20 @@ A (7,-5); B (1,-1) -> 7,21
 */
 
 Console.WriteLine("Please, enter coordinates of two segments : ");
-Console.WriteLine("XA : ");
+Console.Write("XA : ");
 int xa = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("XB : ");
-int xb = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("YA : ");
+Console.Write("YA : ");
 int ya = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("YB : ");
+Console.Write("XB : ");
+int xb = Convert.ToInt32(Console.ReadLine());
+Console.Write("YB : ");
 int yb = Convert.ToInt32(Console.ReadLine());
 
-double ourDistance = Math.Round(Distance(xa, xb, ya, yb), 2, MidpointRounding.ToZero);
+double ourDistance = Math.Round(Distance(xa, ya, xb, yb), 2, MidpointRounding.ToZero);
 Console.WriteLine($"Distance beteen segments with coordinates {xa} , {xb} and {ya}, {yb} is {ourDistance}.");
 
 
-double Distance (int x1, int x2, int y1, int y2)
+double Distance (int x1, int y1, int x2, int y2)
 {
     double distance = Math.Sqrt(Math.Pow((x2-x1), 2) + Math.Pow((y2-y1),2));
     return distance;
