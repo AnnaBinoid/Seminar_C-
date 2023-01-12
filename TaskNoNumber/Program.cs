4ae9d0c684abf99c7c3c274ebc7bcf18b4fa30b2
Anna_Zagrebina <eto-projdet@yandex.ru>
2023-01-12 18:59:36 +0300

    Add TaskNoNumber

---
+++ TaskNoNumber/Program.cs
@@ -0,0 +1,33 @@
+﻿string text = "-Я думаю, - сказал князь, улыбаясь, -что, "
+            + "ежели бы вас послали вместо нашего милого Винценгероде, "
+            + "вы бы взяли приступом согласие прусского короля. "
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+
+// string s = "qwerty"
+//            0123
+// s[3]
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = string.Empty;
+
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if (text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+
+    return result;
+}
+
+String newText = Replace(text, ' ', '|');
+Console.WriteLine(newText);
+Console.WriteLine();
+
+newText = Replace (newText, 'к', 'K');
+Console.WriteLine(newText);
+Console.WriteLine();
+
+
